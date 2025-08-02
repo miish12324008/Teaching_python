0e1fd9c626a0dc842ae01504c8151195fde0aa2e
miish12324008 <151490346+miish12324008@users.noreply.github.com>
2025-08-02 17:23:32 +0530

    Add files via upload

---
+++ day1/day1.py
@@ -0,0 +1,165 @@
+# every thing in a python is object
+
+# each object have its own unique id, type, value
+
+#Mutablity and immutablity 
+
+# mutabe > these changeble
+# Immutable > these are not changable
+
+
+##Immutable
+# num = 2 
+# print(f"my number is {num}") #value
+# print(f'id of num {id(num)}') #id
+# print(f'datatype of num {type(num)}') #dtype
+
+# num = 12
+# print(f'my number is {num}') #updated value
+# print(f'Id of Num is: {id(num)}') #updated id of num
+# print(f'datatype of num is : {type(num)}') #datatype of num
+
+# num = 2 
+# print(f"my number is {num}") #value
+# print(f'id of num {id(num)}') #id
+# print(f'datatype of num {type(num)}') #dtype
+
+##Mutable
+
+# tea = set()
+# print(f'My set is {tea}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'datatype of my set is : {type(tea)}')
+
+# tea.add('black tea')
+# print(f'My set is {tea}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'datatype of my set is : {type(tea)}')
+
+# tea.add('green tea')
+# print(f'My set is {tea}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'datatype of my set is : {type(tea)}')
+
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+# print(f'Id of my set is : {id(tea)}')
+
+
+                                #Datatypes#
+                                
+                                ##Integer##
+# int (whole number) 1,50,6000,74494949
+# boolean (true/false)
+# float (real number) 1.3, 10.6
+# complex number (a+bi)
+
+                            ##integer##
+                    #Operations in integer datatype#
+# sum , diff, multiply, division , floor_division , exponential
+
+##Sum
+a = 6
+b = 10
+# print(a+b)
+# print(a-b)
+# print(a-b)
+
+#Division
+# print(a / b) #simple division
+# print(a//b) # floor division
+
+# print(type(a))
+# print(type(b))
+# print(a/b) 
+# print(a//b)
+# print(round(a/b)) #round value
+# # import math
+# print(math.floor(a/b)) # floor value
+# print(math.ceil(a/b)) #ceil value
+
+#exponential
+
+# print(2**2) #2 X 2
+# print(2**3) # 2 X 2 X 2
+# print(2**5) #2 X 2 X 2 X 2 X 2
+
+##Boolean
+## Output  True / false
+#            1 / 0
+
+# is_boiling = True
+# sugar = True
+# stir = 10
+# ginger = True
+# extra_sugar = False
+
+# print(f'Validate tea : {is_boiling+sugar+stir+ginger+extra_sugar}')
+# print(bool(0))
+# print(bool(1))
+# print(int(True))
+# print(int(False))
+# print(bool(1000000000))
+# print(bool('Manish'))
+# print(bool(-1))
+
+# a = float(1.3)
+# b = float(1.9)
+# print(a)
+# print(int(a))
+
+# print(b)
+# print(int(b))
+
+# num1 = 1
+# print(num1)
+# print(float(num1))
+# print(f'{float(num1):.10f}')
+
+##String 
+
+# customer_name = 'Parshant'
+# order_number = 20
+
+# print(f'serve the tea to customer {customer_name}. his order number is {order_number}')
+
+##Indexing
+# sample_text = "We are learning Python"
+# print(sample_text)
+# print(sample_text[0])
+# print(sample_text[1])
+# print(sample_text[2])
+
+# print(sample_text[-1])
+# print(sample_text[-2])
+
+
+##Slicing
+name = 'manish'
+new_str  = 'MANISH IS TEACHING PYTHON'
+# print(name[0:2:1])
+# print(name[0:3:1])
+# print(name[:3])
+# print(name[0:6:2])
+# print(name[::3])
+# print(name[::])
+# print(name[:3:])
+# print(name[3::])
+
+# print(name[::-1])
+# print(name[:6:-1])
+# print(name[-1:-7:-1])
+# print(name[-6:])
+
+# print(name.capitalize())
+# print(name.upper())
+# print(new_str.lower())
+# print(new_str.capitalize())
+# print(len(name))
+# print(new_str.count('N'))
+# print(new_str.find('N'))
